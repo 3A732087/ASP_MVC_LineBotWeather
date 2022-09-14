2667ba73cd7752bedc0e21fc3e0b30dbc8573479
3A732087 <quannn0180716v@gmail.com>
2022-09-14 21:24:04 +0800

    判斷是否為訊息

--- ASP_MVC_LineBotWeather/Controllers/LineBotController.cs
+++ ASP_MVC_LineBotWeather/Controllers/LineBotController.cs
@@ -31,86 +31,89 @@ namespace ASP_MVC_LineBotWeather.Controllers
                 //剖析JSON
                 var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
 
-                Dictionary<string, object> checkResult = weatherService.SearchCheck(ReceivedMessage.events[0].message.text);
-
-                if (checkResult["Status"].Equals(true))
+                if(ReceivedMessage.events[0].type == "message")
                 {
-                    string JsonStr = "";
-                    string locationName = checkResult["SearchData"].ToString();
-                    string Url = $"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={WeatherAPI}&format=JSON&locationName={locationName}&elementName=MinT,MaxT,PoP";
+                    Dictionary<string, object> checkResult = weatherService.SearchCheck(ReceivedMessage.events[0].message.text);
 
-                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
-                    request.Method = "GET";
-
-                    using (WebResponse response = request.GetResponse())
+                    if (checkResult["Status"].Equals(true))
                     {
-                        using (StreamReader sr = new StreamReader(response.GetResponseStream()))
-                        {
-                            JsonStr = sr.ReadToEnd();
-                        }
-                    }
+                        string JsonStr = "";
+                        string locationName = checkResult["SearchData"].ToString();
+                        string Url = $"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={WeatherAPI}&format=JSON&locationName={locationName}&elementName=MinT,MaxT,PoP";
 
-                    if (!string.IsNullOrEmpty(JsonStr))
-                    {
-                        var resultData = JsonConvert.DeserializeObject<Root>(JsonStr);
-                        if (resultData.success.ToString() == "true")
-                        {
-                            isRock.LineBot.Bot bot = new isRock.LineBot.Bot(ChannelAccessToken);  //傳入Channel access token
-                            List<WeatherElement> Data = new List<WeatherElement>();
-                            List<Time> res = new List<Time>();
-                            Data = resultData.records.location[0].weatherElement;
+                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                        request.Method = "GET";
 
-                            for (int i = 0; i < 3; i++)
+                        using (WebResponse response = request.GetResponse())
+                        {
+                            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
                             {
-                                foreach (var time in Data)
+                                JsonStr = sr.ReadToEnd();
+                            }
+                        }
+
+                        if (!string.IsNullOrEmpty(JsonStr))
+                        {
+                            var resultData = JsonConvert.DeserializeObject<Root>(JsonStr);
+                            if (resultData.success.ToString() == "true")
+                            {
+                                isRock.LineBot.Bot bot = new isRock.LineBot.Bot(ChannelAccessToken);  //傳入Channel access token
+                                List<WeatherElement> Data = new List<WeatherElement>();
+                                List<Time> res = new List<Time>();
+                                Data = resultData.records.location[0].weatherElement;
+
+                                for (int i = 0; i < 3; i++)
                                 {
-                                    res.Add(time.time[i]);
+                                    foreach (var time in Data)
+                                    {
+                                        res.Add(time.time[i]);
+                                    }
                                 }
+
+                                //順序：降雨機率、最低溫、最高溫
+                                Uri MessageImg = new Uri("圖片網址");
+
+                                Column Columns = new Column();
+                                CarouselTemplate carouselTemplate = new CarouselTemplate();
+
+                                //組訊息
+                                string title = "";
+                                string content = "";
+
+                                var actions = new List<isRock.LineBot.TemplateActionBase>();
+                                actions.Add(new isRock.LineBot.UriAction() { label = "詳細內容", uri = new Uri("https://www.cwb.gov.tw/V8/C/W/County/index.html") });
+
+
+                                for (int j = 0; j < res.Count(); j += 3)
+                                {
+                                    title = Convert.ToDateTime(res[j].startTime).ToString("MM-dd HH:mm") + " ~ " + Convert.ToDateTime(res[j].endTime).ToString("MM-dd HH:mm");
+                                    content = "溫度" + res[j + 1].parameter.parameterName + " ~ " + res[j + 2].parameter.parameterName + "°C" +
+                                        " \n" + "降雨機率" + res[j].parameter.parameterName + "%";
+
+                                    Columns = new Column { thumbnailImageUrl = MessageImg, title = title, text = content, actions = actions };
+                                    carouselTemplate.columns.Add(Columns);
+                                }
+                                isRock.LineBot.Utility.ReplyTemplateMessage(ReceivedMessage.events[0].replyToken, carouselTemplate, ChannelAccessToken);
                             }
-
-                            //順序：降雨機率、最低溫、最高溫
-                            Uri MessageImg = new Uri("圖片網址");
-
-                            Column Columns = new Column();
-                            CarouselTemplate carouselTemplate = new CarouselTemplate();
-
-                            //組訊息
-                            string title = "";
-                            string content = "";
-
-                            var actions = new List<isRock.LineBot.TemplateActionBase>();
-                            actions.Add(new isRock.LineBot.UriAction() { label = "詳細內容", uri = new Uri("https://www.cwb.gov.tw/V8/C/W/County/index.html") });
-
-
-                            for (int j = 0; j < res.Count(); j += 3)
-                            {
-                                title = Convert.ToDateTime(res[j].startTime).ToString("MM-dd HH:mm") + " ~ " + Convert.ToDateTime(res[j].endTime).ToString("MM-dd HH:mm");
-                                content = "溫度" + res[j + 1].parameter.parameterName + " ~ " + res[j + 2].parameter.parameterName + "°C" +
-                                    " \n" + "降雨機率" + res[j].parameter.parameterName + "%";
-
-                                Columns = new Column { thumbnailImageUrl = MessageImg, title = title, text = content, actions = actions };
-                                carouselTemplate.columns.Add(Columns);
-                            }
-                            isRock.LineBot.Utility.ReplyTemplateMessage(ReceivedMessage.events[0].replyToken, carouselTemplate, ChannelAccessToken);
                         }
-                    }
-                }
-                else
-                {
-                    string erroeMsg = "";
-                    if(checkResult["Msg"].ToString() == "搜尋格式錯誤！")
-                    {
-                        erroeMsg = checkResult["Msg"].ToString() + 
-                            "\n若要查詢氣象資訊請輸入" +
-                            "\n『天氣 縣市名稱』" +
-                            "\n例如：天氣 臺中市";
                     }
                     else
                     {
-                        erroeMsg = checkResult["Msg"].ToString();
-                    }
-                    isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, erroeMsg, ChannelAccessToken);
+                        string erroeMsg = "";
+                        if (checkResult["Msg"].ToString() == "搜尋格式錯誤！")
+                        {
+                            erroeMsg = checkResult["Msg"].ToString() +
+                                "\n若要查詢氣象資訊請輸入" +
+                                "\n『天氣 縣市名稱』" +
+                                "\n例如：天氣 臺中市";
+                        }
+                        else
+                        {
+                            erroeMsg = checkResult["Msg"].ToString();
+                        }
+                        isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, erroeMsg, ChannelAccessToken);
 
+                    }
                 }
                 return Ok();
 
